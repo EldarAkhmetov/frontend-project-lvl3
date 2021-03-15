a6b6e997a077c1e2e96b86dc73a2fffc00dfab8e
Eldar Akhmetov <ahmetov20@yandex.ru>
2021-03-15 22:26:28 +0700

    one more fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
+  entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
