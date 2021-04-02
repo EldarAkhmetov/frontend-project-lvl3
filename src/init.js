39b5f32acb29f614d0e0cc7be8fc76aa1a05bcf2
Eldar Akhmetov <ahmetov20@yandex.ru>
2021-04-02 17:23:57 +0700

    try to fix

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+import { cloneDeep } from 'lodash';
 import Main from './Main.js';
 
 const initialState = {
@@ -12,6 +13,6 @@ const initialState = {
 
 export default () => {
   const element = document.getElementById('point');
-  const obj = new Main(element, initialState);
+  const obj = new Main(element, cloneDeep(initialState));
   obj.init();
 };
