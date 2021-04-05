3ab7d6c152873eaf301b1ad20b8dcf3f465b994e
Eldar Akhmetov <ahmetov20@yandex.ru>
2021-04-05 23:51:00 +0700

    try to fix font-weight

--- src/templates/article-item.js
+++ src/templates/article-item.js
@@ -1,6 +1,6 @@
 export default (article) => `
   <li class="list-group-item d-flex justify-content-between align-items-start" data-id=${article.id}>
-    <a href=${article.link} class="${article.isRead ? 'fw-normal' : 'fw-bold'}" target="_blank" rel="noopener noreferrer">${article.title}</a>
+    <a href=${article.link} class="${article.isRead ? 'font-weight-normal' : 'font-weight-bold'}" target="_blank" rel="noopener noreferrer">${article.title}</a>
     <button type="button" class="btn btn-primary btn-sm">Просмотр</button>
   </li>
 `;
