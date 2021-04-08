8b5dd769fc1379907237141d87bae065202f7873
Eldar Akhmetov <ahmetov20@yandex.ru>
2021-04-08 16:49:45 +0700

    add autoupdate

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import onChange from 'on-change';
 import * as yup from 'yup';
-import render, { renderSubmit, renderFeedback } from './view.js';
+import loadRss from './rss-loader';
+import parseRss from './rss-parser';
+import render, {
+  renderSubmit,
+  renderFeedback,
+  renderFeed,
+  renderArticles,
+} from './view.js';
 
 export default (i18n) => {
   const initialState = {
@@ -26,34 +33,91 @@ export default (i18n) => {
     modalCloseButtons: document.querySelectorAll('[data-dismiss="modal"]'),
   };
 
-  const watchedState = onChange(initialState, (path) => {
+  const watchedState = onChange(initialState, (path, value) => {
     switch (path) {
       case 'form.state':
-        renderSubmit(watchedState.form.state, element);
+        renderSubmit(value, element);
         break;
       case 'form.message':
-        renderFeedback(watchedState.form.message, element);
+        renderFeedback(value, element);
+        break;
+      case 'feeds':
+        renderFeed(value);
+        break;
+      case 'articles':
+        renderArticles(value);
         break;
       default:
-        console.log(path);
-        break;
+        throw new Error('No such property');
     }
   });
 
-  const schema = yup.string()
-    .url(i18n.t('errorMessages.invalidUrl'))
-    .notOneOf(watchedState.feeds.map((feed) => feed.link), i18n.t('errorMessages.alreadyExists'));
+  const initAutoUpdate = (state) => {
+    const interval = 5000;
+    const updateArticles = () => {
+      const currentFeedLinks = state.feeds.map((feed) => feed.link);
+      Promise.all(currentFeedLinks.map(loadRss))
+        .then((data) => {
+          const currentArticles = state.articles;
+          const newArticles = [];
+          const parsedData = data.map((feed) => parseRss(feed));
+          parsedData.forEach((feed) => {
+            feed.items.forEach((item) => {
+              const hasArticle = currentArticles.some((article) => article.link === item.link);
+              if (!hasArticle) {
+                newArticles.push(item);
+              }
+            });
+          });
+          if (newArticles.length > 0) {
+            watchedState.articles = [...newArticles, ...currentArticles];
+          }
+          setTimeout(updateArticles, interval);
+        })
+        .catch(() => {
+        });
+    };
+    setTimeout(updateArticles, interval);
+  };
 
   element.form.addEventListener('submit', (e) => {
     e.preventDefault();
-    schema.validate(element.input.value)
-      .then(() => {
-        watchedState.form.message = '';
-        watchedState.form.state = 'loading';
+    const { feedback } = element;
+    const { value } = element.input;
+    watchedState.form.message = '';
+    try {
+      const schema = yup.string()
+        .url(i18n.t('errorMessages.invalidUrl'))
+        .notOneOf(watchedState.feeds.map(({ link }) => link), i18n.t('errorMessages.alreadyExists'));
+      schema.validateSync(value);
+    } catch (error) {
+      const [errorText] = error.errors;
+      watchedState.form.message = errorText;
+      return;
+    }
+
+    watchedState.form.state = 'loading';
+    loadRss(value)
+      .then((data) => {
+        const parsedData = parseRss(data);
+        watchedState.form.state = 'filling';
+        element.input.value = '';
+        console.log(parsedData);
+        feedback.classList.remove('text-danger');
+        watchedState.form.message = i18n.t('successMessages.feedLoaded');
+        const { title, description, items } = parsedData;
+        watchedState.feeds = [{ title, description, link: value }, ...watchedState.feeds];
+        watchedState.articles = [...items, ...watchedState.articles];
+        if (watchedState.feeds.length === 1) {
+          initAutoUpdate(watchedState);
+        }
       })
       .catch((error) => {
-        const [errorText] = error.errors;
-        watchedState.form.message = errorText;
+        watchedState.form.state = 'filling';
+        watchedState.form.message = i18n.t('errorMessages.rssRequired');
+        if (error.message !== "Cannot read property 'textContent' of null") {
+          watchedState.form.message = i18n.t('errorMessages.networkError');
+        }
       });
   });
 };
--- src/view.js
+++ src/view.js
@@ -1,4 +1,8 @@
 import mainInnerHtml from './templates/main-inner-html';
+import mainFeedHtml from './templates/main-feed-html';
+import createFeedItem from './templates/feed-item';
+import mainArticleHtml from './templates/main-article-html';
+import createArticleItem from './templates/article-item';
 
 const renderDisableSubmit = (element) => {
   const { input, submitButton } = element;
@@ -23,10 +27,56 @@ export const renderSubmit = (formState, element) => {
   }
 };
 
+export const renderFeed = (feeds) => {
+  console.log(feeds);
+  const section = document.querySelector('section[name="feed"]');
+  const row = section.querySelector('div[name="feed-info"]');
+  row.innerHTML = mainFeedHtml;
+  const feedInfo = row.querySelector('ul[name="feed-list"]');
+  const feedList = feeds.map(createFeedItem)
+    .join('');
+  feedInfo.innerHTML = feedList;
+};
+
+export const renderArticles = (articleItems) => {
+  const section = document.querySelector('section[name="feed"]');
+  const row = section.querySelector('div[name="articles"]');
+  row.innerHTML = mainArticleHtml;
+  const articles = row.querySelector('ul[name="articles-list"]');
+  const articlesList = articleItems
+    .map(createArticleItem)
+    .join('');
+  articles.innerHTML = articlesList;
+  const viewButtons = articles.querySelectorAll('button');
+  const articleLinks = articles.querySelectorAll('a');
+  articleLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      const articleId = e.target.parentNode.dataset.id;
+      const currentArticle = articleItems
+        .find((article) => article.id === articleId);
+      if (!currentArticle.isRead) {
+        currentArticle.isRead = true;
+      }
+    });
+  });
+  viewButtons.forEach((viewButton) => {
+    viewButton.addEventListener('click', (e) => {
+      const articleId = e.target.parentNode.dataset.id;
+      const currentArticle = articleItems
+        .find((article) => article.id === articleId);
+      if (!currentArticle.isRead) {
+        currentArticle.isRead = true;
+      }
+      //renderModal(currentArticle);
+    });
+  });
+};
+
 export const renderFeedback = (message, element) => {
   const { feedback } = element;
   if (!message) {
     feedback.classList.add('d-none');
+    feedback.classList.add('text-danger');
     feedback.textContent = '';
     return;
   }
